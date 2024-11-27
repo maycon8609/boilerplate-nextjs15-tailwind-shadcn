ef4ff898cc8c1a68a978da3612807f86bb42c281
maycon silva <maycon8609@gmail.com>
2024-11-27 19:55:15 -0300

    refactor: prettier format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <div className="flex flex-col items-center">
         <h1 className="text-5xl font-bold">Hello World</h1>
       </div>
