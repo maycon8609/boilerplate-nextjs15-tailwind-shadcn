64013eb1f51b38fb057ddb5aa5caca81504d5280
maycon silva <maycon8609@gmail.com>
2024-11-28 14:51:50 -0300

    chore: configure shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from '@/components/ui/card';
+
 export default function Home() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <div className="flex flex-col items-center">
-        <h1 className="text-5xl font-bold">Hello World</h1>
-      </div>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      <Card>
+        <CardHeader>
+          <CardDescription>
+            <h1 className="text-2xl font-bold">Header</h1>
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <h1 className="text-5xl font-bold">Hello World</h1>
+        </CardContent>
+
+        <CardFooter>
+          <h1 className="text-2xl font-bold">Footer</h1>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,23 @@
+/* eslint-disable unicorn/prefer-module */
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
